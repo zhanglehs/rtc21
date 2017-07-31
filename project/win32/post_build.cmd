41d574eb81d2e3e487f626f7402033907a65b1cd
zhangle <zl124548@youku.com>
2017-07-31 20:30:36 +0800

    windows compile passed

--- project/win32/post_build.cmd
+++ project/win32/post_build.cmd
@@ -1,17 +1,12 @@
 set ProjectDir=%1%
-set SolutionDir=%2%
+set ProjectName=%2%
 set Configuration=%3%
 
-set SRC=%ProjectDir%..\..\..\src
-set Include=%ProjectDir%..\..\..\out\%Configuration%\include
-set Lib=%ProjectDir%..\..\..\out\%Configuration%\lib\
-set Bin=%ProjectDir%..\..\..\out\%Configuration%\bin\
-set Pdb=%ProjectDir%..\..\..\out\%Configuration%\pdb\
-::set Test=%ProjectDir%..\..\..\test\out\%Configuration%\
+set Out=%ProjectDir%..\..\out\%Configuration%
+xcopy %ProjectDir%..\..\src\engine_api\*.h %Out%\include\ /s /y
+xcopy %ProjectDir%\%Configuration%\*.lib %Out%\lib\ /s /y
+xcopy %ProjectDir%\%Configuration%\%ProjectName%.pdb %Out%\lib\ /s /y
+xcopy %ProjectDir%\%Configuration%\*.dll %Out%\lib\ /s /y
 
-xcopy %SRC%\engine_api\*.h %Include%\ /s /y
-xcopy %SolutionDir%\%Configuration%\*.lib %Lib% /s /y
-xcopy %SolutionDir%\%Configuration%\*.pdb %Pdb% /s /y
-xcopy %SolutionDir%\%Configuration%\*.dll %Bin% /s /y
-::xcopy %SolutionDir%\%Configuration%\*.dll %Test% /s /y
+xcopy %ProjectDir%\%Configuration%\*.dll %ProjectDir%\out\%Configuration%\ /s /y
 
